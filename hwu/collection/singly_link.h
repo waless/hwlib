056dc2cae167b601e56eb03310ae411e50d35422
waless <waless.seel@gmail.com>
2012-03-26 00:41:16 +0900

    delete moved files

--- hwu/collection/singly_link.h
+++
@@ -1,9 +0,0 @@
-#ifndef HWU_SINGLY_LINK_H_
-#define HWU_SINGLY_LINK_H_
-
-typedef struct tag_hwu_singly_link_t
-{
-	struct tag_hwu_singly_link_t* next;
-} hwu_singly_link_t;
-	
-#endif  /* HWU_SINGLY_LINK_H_ */
