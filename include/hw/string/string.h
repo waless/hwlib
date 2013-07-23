4b21ecb0a55ee1a307354892b51d2ac8dd501368
waless <waless.seel@gmail.com>
2013-07-23 20:48:54 +0900

    working up

--- include/hw/string/string.h
+++ include/hw/string/string.h
@@ -23,6 +23,10 @@ extern void hw_string_append_buffer(hw_string_t* state, const char* buffer, hwu3
 extern hwbool hw_string_find_char(hwu32* char_index, const hw_string_t* state);
 extern hwbool hw_string_find_string(hwu32* string_index, hwu32* string_size, const hw_string_t* state, const hw_string_t* string);
 extern hwbool hw_string_find_buffer(hwu32* string_index, hwu32* string_size, const hw_string_t* state, const char* buffer, hwu32 size);
+extern hwbool hw_string_substring_to_string(hw_string_t* out, const hw_string_t* state, hwu32 begin_index, hwu32 length);
+extern hwbool hw_string_substring_to_buffer(char* buffer, hwu32 buffer_size, const hw_string_t* state, hwu32 begin_index, hwu32 length);
+extern hwu32 hw_string_get_split_count(const hw_string_t* state, char separete);
+extern void hw_string_split(
 
 #endif
 
