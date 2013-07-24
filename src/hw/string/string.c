edc8ee20fb76442c93df362f33d8a309dd0959d9
waless <waless.seel@gmail.com>
2013-07-25 02:57:21 +0900

    working up

--- src/hw/string/string.c
+++ src/hw/string/string.c
@@ -1,28 +1,45 @@
 ﻿#include "hw/string/string.h"
+#include <stddef.h>
 #include "hw/types.h"
 
 void hw_string_initialize(hw_string_t* state, char* buffer, hwu32 size)
 {
+    state->buffer   = buffer;
+    state->capacity = size;
+    state->length   = 0;
+
+    if(buffer != NULL && size > 0) {
+        state->length = strlen(buffer);
+    }
 }
 
 void hw_string_finalize(hw_string_t* state)
 {
+    state->buffer   = NULL;
+    state->capacity = 0;
+    state->length   = 0;
 }
 
 char hw_string_at(const hw_string_t* state, hwu32 index)
 {
+    return (index < state->length) ? state->buffer[index] : "\0";
 }
 
 char* hw_string_get_pointer(hw_string_t* state, hwu32 index)
 {
+    return (index < state->length) ? state->buffer + index : NULL;
 }
 
 const char* hw_string_get_const_pointer(const hw_string_t* state, hwu32 index)
 {
+    return (index < state->length) ? state->buffer + index : NULL;
 }
 
 void hw_string_clear(hw_string_t* state)
 {
+    if(state->buffer != NULL && state->capacity > 0) {
+        memset(state->buffer, 0, sizeof(char) * state->capacity);
+    }
 }
 
 void hw_string_copy_string(hw_string_t* lhs, const hw_string_t* rhs)
@@ -33,13 +50,43 @@ void hw_string_copy_buffer(hw_string_t* lhs, const char* buffer, hwu32 size)
 {
 }
 
-void hw_string_append_string(hw_string_t* state, const hw_string_t* rhs);
-void hw_string_append_char(hw_string_t* state, char c);
-void hw_string_append_buffer(hw_string_t* state, const char* buffer, hwu32 size);
-hwbool hw_string_find_char(hwu32* char_index, const hw_string_t* state);
-hwbool hw_string_find_string(hwu32* string_index, hwu32* string_size, const hw_string_t* state, const hw_string_t* string);
-hwbool hw_string_find_buffer(hwu32* string_index, hwu32* string_size, const hw_string_t* state, const char* buffer, hwu32 size);
-hwbool hw_string_substring_to_string(hw_string_t* out, const hw_string_t* state, hwu32 begin_index, hwu32 length);
-hwbool hw_string_substring_to_buffer(char* buffer, hwu32 buffer_size, const hw_string_t* state, hwu32 begin_index, hwu32 length);
-hwu32 hw_string_get_split_count(const hw_string_t* state, char separete);
-hwu32 hw_string_split(hw_string_t out_array[], hwu32 out_array_count, const hw_string_t* state, char separete);
+void hw_string_append_string(hw_string_t* state, const hw_string_t* rhs)
+{
+}
+
+void hw_string_append_char(hw_string_t* state, char c)
+{
+}
+
+void hw_string_append_buffer(hw_string_t* state, const char* buffer, hwu32 size)
+{
+}
+
+hwbool hw_string_find_char(hwu32* char_index, const hw_string_t* state)
+{
+}
+
+hwbool hw_string_find_string(hwu32* string_index, hwu32* string_size, const hw_string_t* state, const hw_string_t* string)
+{
+}
+
+hwbool hw_string_find_buffer(hwu32* string_index, hwu32* string_size, const hw_string_t* state, const char* buffer, hwu32 size)
+{
+}
+
+hwbool hw_string_substring_to_string(hw_string_t* out, const hw_string_t* state, hwu32 begin_index, hwu32 length)
+{
+}
+
+hwbool hw_string_substring_to_buffer(char* buffer, hwu32 buffer_size, const hw_string_t* state, hwu32 begin_index, hwu32 length)
+{
+}
+
+hwu32 hw_string_get_split_count(const hw_string_t* state, char separete)
+{
+}
+
+hwu32 hw_string_split(hw_string_t out_array[], hwu32 out_array_count, const hw_string_t* state, char separete)
+{
+}
+
