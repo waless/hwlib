3c55ce818f57859a814474db06d7172cac656f93
waless <waless.seel@gmail.com>
2013-07-25 20:07:33 +0900

    working up

--- src/hw/string/string.c
+++ src/hw/string/string.c
@@ -44,22 +44,38 @@ void hw_string_clear(hw_string_t* state)
 
 void hw_string_copy_string(hw_string_t* lhs, const hw_string_t* rhs)
 {
+    hw_string_copy_buffer(lhs, rhs->buffer, rhs->length);
 }
 
 void hw_string_copy_buffer(hw_string_t* lhs, const char* buffer, hwu32 size)
 {
+    if(size < lhs->capacity) {
+        memcpy(lhs->buffer, rhs->buffer, size);
+        lhs->buffer[size] = '\0';
+        lhs->length = size;
+    }
+    else {
+        memcpy(lhs->buffer, buffer, lhs->capacity);
+        lhs->buffer[lhs->capacity - 1] = '\0';
+        lhs->length = lhs->capacity - 1;
+    }
 }
 
 void hw_string_append_string(hw_string_t* state, const hw_string_t* rhs)
 {
+    hw_string_append_buffer(state, rhs->buffer, rhs->length);
 }
 
 void hw_string_append_char(hw_string_t* state, char c)
 {
+    if(state->length < (state->capacity - 1)) {
+        state->buffer[state->length++] = c;
+    }
 }
 
 void hw_string_append_buffer(hw_string_t* state, const char* buffer, hwu32 size)
 {
+    if(state->capacity - state->length >jj
 }
 
 hwbool hw_string_find_char(hwu32* char_index, const hw_string_t* state)
