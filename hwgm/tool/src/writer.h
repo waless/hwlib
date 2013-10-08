d707a42d111670a77e35dab73e543ee369ca6511
waless <waless.seel@gmail.com>
2013-10-08 22:35:11 +0900

    working up

--- hwgm/tool/src/writer.h
+++ hwgm/tool/src/writer.h
@@ -7,30 +7,40 @@
 extern "C" {
 #endif
 
+typedef struct hwgm_vertex_t {
+    hwu16 vertex_count;
+    hwf32 vertices[];
+} hwgm_vertex_t;
+
+typedef struct hwgm_mesh_t {
+    hwu16 index_count;
+    hwu16 indices_offset;
+} hwgm_mesh_t;
+
+typedef struct hwgm_node_t {
+    hwm_matrix44_t transform;
+    hwu16          mesh_count;
+    hwu32          mesh_offsets[];
+} hwgm_node_t;
+
 typedef struct hwgm_model_t {
     hws8  code[4];
     hwu8  major_version;
     hwu8  minor_version;
 
-    hwu32 node_count;
+    hwu16 node_count;
     hwu32 node_offset;
 
-    hwu32 mesh_count;
+    hwu16 mesh_count;
     hwu32 mesh_offset;
     
-    hwu32 vertex_count;
+    hwu16 vertex_count;
     hwu32 vertex_offset;
 
     hwgm_node_t   nodes[];
     hwgm_mesh_t   meshes[];
     hwgm_vertex_t vertices[];
 } hwgm_model_t;
-
-typedef struct hwgm_node_t {
-} hwgm_node_t;
-
-typedef struct hwgm_mesh_t {
-} hwgm_mesh_t;
 
 #ifdef __cplusplus
 }
