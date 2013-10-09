85da01b29bdcbd00820f12627e05ff297180d96c
waless <waless.seel@gmail.com>
2013-10-09 22:16:30 +0900

    add api prottype to fixed32

--- hwm/include/hwm/fixed32.h
+++ hwm/include/hwm/fixed32.h
@@ -3,7 +3,41 @@
 
 #include <hw/types.h>
 
-typedef hws32 hwm_fixed32;
+typedef hws32 hwm_fixed32_t;
+
+extern const hwm_fixed32_t k_hwm_fixed32_one;
+extern const hwm_fixed32_t k_hwm_fixed32_half;
+extern const hwm_fixed32_t k_hwm_fixed32_pi;
+extern const hwm_fixed32_t k_hwm_fixed32_two_pi;
+
+extern hwm_fixed32_t hwm_fixed32_add(hwm_fixed32_t lhs, hwm_fixed32_t rhs);
+extern hwm_fixed32_t hwm_fixed32_sub(hwm_fixed32_t lhs, hwm_fixed32_t rhs);
+extern hwm_fixed32_t hwm_fixed32_mul(hwm_fixed32_t lhs, hwm_fixed32_t rhs);
+extern hwm_fixed32_t hwm_fixed32_div(hwm_fixed32_t lhs, hwm_fixed32_t rhs);
+extern hwm_fixed32_t hwm_fixed32_mod(hwm_fixed32_t lhs, hwm_fixed32_t rhs);
+
+extern hwm_fixed32-t hwm_fixed32_sin(hwm_fixed32_t x);
+extern hwm_fixed32_t hwm_fixed32_cos(hwm_fixed32_t x);
+extern hwm_fixed32_t hwm_fixed32_tan(hwm_fixed32_t x);
+extern hwm_fixed32_t hwm_fixed32_asin(hwm_fixed32_t x);
+extern hwm_fixed32_t hwm_fixed32_acos(hwm_fixed32_t x);
+extern hwm_fixed32_t hwm_fixed32_atan(hwm_fixed32_t x);
+extern hwm_fixed32_t hwm_fixed32_atan2(hwm_fixed32_t x, hwm_fix3d32_t y);
+
+extern hwm_fixed32_t hwm_fix3d32_ceil(hwm_fixed32_t value);
+extern hwm_fixed32_t hwm_fixed32_floor(hwm_fixed32_t value);
+extern hwm_fixed32_t hwm_fixed32_sqrt(hwm_fixed32_t value);
+extern hwm_fixed32_t hwm_fixed32_abs(hwm_fixed32_t value);
+
+extern hws32 hwm_fixed32_to_s32(hwm_fixed32_t value);
+extern hws64 hwm_fixed32_to_s64(hwm_fixed32_t value);
+extern hwf32 hwm_fixed32_to_f32(hwm_fixed32_t value);
+extern hwf64 hwm_fixed32_to_f64(hwm_fixed32_t value);
+
+extern hwm_fixed32_t hwm_fixed32_from_s32(hwm_fixed32_t value);
+extern hwm_fixed32_t hwm_fixed32_from_s64(hwm_fixed32_t value);
+extern hwm_fixed32_t hwm_fixed32_from_f32(hwf32 value);
+extern hwm_fixed32_t hwm_fixed32_from_f64(hwf64 value);
 
 #endif
 
