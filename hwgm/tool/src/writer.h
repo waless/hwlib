5f2a3e24ddb10e9bbf04f0189965b36bc2d377fa
waless <waless.seel@gmail.com>
2013-11-25 01:10:48 +0900

    working up

--- hwgm/tool/src/writer.h
+++ hwgm/tool/src/writer.h
@@ -3,10 +3,15 @@
 
 #include <hw/types.h>
 #include <hwcp/type.h>
+#include "reader.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+extern void writer_initialize(reader_t* reader);
+extern void writer_finalize(writer_t* writer);
+extern void writer_run(writer_t* writer);
 
 #ifdef __cplusplus
 }
