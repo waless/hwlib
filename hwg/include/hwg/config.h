a2aff885aef80ad39626ffde6001a02cddfa308c
waless <waless.seel@gmail.com>
2013-11-29 22:51:41 +0900

    working up

--- hwg/include/hwg/config.h
+++ hwg/include/hwg/config.h
@@ -1,2 +1,12 @@
-
 #ifndef HWG_CONFIG_H_
+#define HWG_CONFIG_H_
+
+#define HWG_CONFIG_DX_11_PROFILE_9_3
+
+#define HWG_CONFIG_GL_4_0
+#define HWG_CONFIG_GL_ES_1_1
+#define HWG_CONFIG_GL_ES_2_0
+#define HWG_CONFIG_GL_ES_3_0
+
+#endif
+
