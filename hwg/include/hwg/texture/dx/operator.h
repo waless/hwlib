b356ebb041d6db0d505ad1c74e8fba4a70195cc7
waless <waless.seel@gmail.com>
2013-12-07 21:11:30 +0900

    working up

--- hwg/include/hwg/texture/dx/operator.h
+++ hwg/include/hwg/texture/dx/operator.h
@@ -3,21 +3,47 @@
 
 #include "hwg/dx.h"
 
-typedef enum hwg_surface_format_t {
-    HWG_SURFACE_FORMAT_UNKNOWN = -1,
-    HWG_SURFACE_FORMAT_R8G8B8,
-    HWG_SURFACE_FORMAT_R8G8B8A8,
-    HWG_SURFACE_FORMAT_D24S8,
-    HWG_SURFACE_FORMAT_R5G6B5,
-    HWG_SURFACE_FORMAT_R4G4B4A4,
-    HWG_SURFACE_FORMAT_D16,
-    HWG_SURFACE_FORMAT_A8,
-    HWG_SURFACE_DXT1,
-    HWG_SURFACE_DXT3,
-    HWG_SURFACE_DXT5,
-    HWG_SURFACE_ETC1,
-    HWG_SURFACE_ETC2,
-} hwg_surface_format_t;
+/* @enum 色フォーマット列挙型
+ *
+ * @brief 色の構成要素の識別子です
+ *        この識別子は環境別ではなく
+ *        ライブラリが認識する識別子です
+ */
+typedef enum hwg_color_format_t {
+    HWG_COLOR_FORMAT_UNKNOWN = -1,
+    HWG_COLOR_FORMAT_R8G8B8,
+    HWG_COLOR_FORMAT_R8G8B8A8,
+    HWG_COLOR_FORMAT_D24S8,
+    HWG_COLOR_FORMAT_R5G6B5,
+    HWG_COLOR_FORMAT_R4G4B4A4,
+    HWG_COLOR_FORMAT_D16,
+    HWG_COLOR_FORMAT_A8,
+    HWG_COLOR_FORMAT_P8,
+    HWG_COLOR_FORMAT_DXT1,
+    HWG_COLOR_FORMAT_DXT3,
+    HWG_COLOR_FORMAT_DXT5,
+    HWG_COLOR_FORMAT_ETC1,
+    HWG_COLOR_FORMAT_ETC2,
+} hwg_color_format_t;
+
+/* @enum テクスチャフォーマット列挙型
+ *
+ * @brief テクスチャフォーマット識別子です
+ *        この識別子は抽象化されてもので、
+ *        ライブラリが環境差を吸収する意図で
+ *        指定されるものです
+ *        詳細な指定はhwg_color_format_tを
+ *        使用します
+ */
+typedef enum hwg_texture_format_t {
+    HWG_TEXTURE_FORMAT_UNKNOWN = -1,
+    HWG_TEXTURE_FORMAT_FULL_COLOR,
+    HWG_TEXTURE_FORMAT_TRUE_COLOR,
+    HWG_TEXTURE_FORMAT_COMPRESS_NONE_ALPHA,
+    HWG_TEXTURE_FORMAT_COMPRESS_WITH_ALPHA,
+    HWG_TEXTURE_FORMAT_DEPTH,
+    HWG_TEXTURE_FORMAT_DEPTH_WITH_STENSIL,
+} hwg_texture_format_t;
 
 typedef enum hwg_texture_wrap_t {
     HWG_TEXTURE_ADDRESS_REPEAT,
