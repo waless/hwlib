f2a4d3cdaef976c54fa1258c81c04dbcc0e5418e
waless <waless.seel@gmail.com>
2013-12-08 21:12:51 +0900

    working up

--- hwg/include/hwg/texture/dx/operator.h
+++ hwg/include/hwg/texture/dx/operator.h
@@ -3,47 +3,33 @@
 
 #include "hwg/dx.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* @enum 色フォーマット列挙型
  *
  * @brief 色の構成要素の識別子です
  *        この識別子は環境別ではなく
  *        ライブラリが認識する識別子です
  */
-typedef enum hwg_color_format_t {
-    HWG_COLOR_FORMAT_UNKNOWN = -1,
-    HWG_COLOR_FORMAT_R8G8B8,
-    HWG_COLOR_FORMAT_R8G8B8A8,
-    HWG_COLOR_FORMAT_D24S8,
-    HWG_COLOR_FORMAT_R5G6B5,
-    HWG_COLOR_FORMAT_R4G4B4A4,
-    HWG_COLOR_FORMAT_D16,
-    HWG_COLOR_FORMAT_A8,
-    HWG_COLOR_FORMAT_P8,
-    HWG_COLOR_FORMAT_DXT1,
-    HWG_COLOR_FORMAT_DXT3,
-    HWG_COLOR_FORMAT_DXT5,
-    HWG_COLOR_FORMAT_ETC1,
-    HWG_COLOR_FORMAT_ETC2,
-} hwg_color_format_t;
-
-/* @enum テクスチャフォーマット列挙型
- *
- * @brief テクスチャフォーマット識別子です
- *        この識別子は抽象化されてもので、
- *        ライブラリが環境差を吸収する意図で
- *        指定されるものです
- *        詳細な指定はhwg_color_format_tを
- *        使用します
- */
-typedef enum hwg_texture_format_t {
-    HWG_TEXTURE_FORMAT_UNKNOWN = -1,
-    HWG_TEXTURE_FORMAT_FULL_COLOR,
-    HWG_TEXTURE_FORMAT_TRUE_COLOR,
-    HWG_TEXTURE_FORMAT_COMPRESS_NONE_ALPHA,
-    HWG_TEXTURE_FORMAT_COMPRESS_WITH_ALPHA,
-    HWG_TEXTURE_FORMAT_DEPTH,
-    HWG_TEXTURE_FORMAT_DEPTH_WITH_STENSIL,
-} hwg_texture_format_t;
+typedef enum hwg_pixel_format_t {
+    HWG_PIXEL_FORMAT_UNKNOWN,
+    HWG_PIXEL_FORMAT_R8G8B8,
+    HWG_PIXEL_FORMAT_R8G8B8A8,
+    HWG_PIXEL_FORMAT_R5G6B5,
+    HWG_PIXEL_FORMAT_R4G4B4A4,
+    HWG_PIXEL_FORMAT_D24S8,
+    HWG_PIXEL_FORMAT_D16,
+    HWG_PIXEL_FORMAT_A8,
+    HWG_PIXEL_FORMAT_P8,
+    HWG_PIXEL_FORMAT_DXT1,
+    HWG_PIXEL_FORMAT_DXT3,
+    HWG_PIXEL_FORMAT_DXT5,
+    HWG_PIXEL_FORMAT_ETC1,
+    HWG_PIXEL_FORMAT_ETC2,
+    HWG_PIXEL_FORMAT_NUM,
+} hwg_pixel_format_t;
 
 typedef enum hwg_texture_wrap_t {
     HWG_TEXTURE_ADDRESS_REPEAT,
@@ -62,6 +48,10 @@ typedef enum hwg_texture_filter_t {
     HWG_TEXTURE_FILTER_POINT,
     HWG_TEXTURE_FILTER_LINEAR,
 } hwg_texture_filter_t;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
