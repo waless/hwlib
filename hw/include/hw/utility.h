2d8204b616937284fb200e813ef7728665239a34
waless <waless.seel@gmail.com>
2014-05-14 19:49:45 +0900

    HW_MINマクロが常に右辺を返すよ運になっていたのを修正

--- hw/include/hw/utility.h
+++ hw/include/hw/utility.h
@@ -6,7 +6,7 @@
 #define HW_ALIGNED_ROUND_DOWN(address, alignment) ((address) & ~((alignment) - 1))
 
 #define HW_MAX(a, b) (((a) > (b)) ? (a) : (b))
-#define HW_MIN(a, b) (((a) < (b)) ? (b) : (b))
+#define HW_MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 #define HW_SAFE_FREE(p)   if((p) != NULL) { hw_free((p)); (p) = NULL; }
 #define HW_SAFE_DELETE(p) if((p) != NULL) { delete p; (p) = NULL; }
