5ebf8b42e49ca3375dbe3b4a8941336d211c2bac
waless <waless.seel@live.jp>
2014-10-20 22:16:58 +0900

    hwg:ひとまずwindows版作成開始

--- hwg/core/src/hwg.c
+++ hwg/core/src/hwg.c
@@ -1,8 +1,14 @@
 #include "hwg.h"
+#include <hw/types.h>
+#include <hw/error.h>
 #include "hwg/dx.h"
 
-hwbool hwg_initialize()
+hwbool hwg_initialize(const hwg_parameter_t* param)
 {
+    HRESULT result = S_OK;
+    RECT rc;
+
+    HW_ASSERT(param->hwnd != NULL);
 }
 
 hwbool hwg_finalize()
